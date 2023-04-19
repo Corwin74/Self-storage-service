4748fe3eb84a0a9bda7cc0070f6f3fe9209f3be2
Victor Abdukarimov <viredmi@gmail.com>
2023-04-19 13:55:48 +0300

    User data from db in template

--- storage_service/userapp/models.py
+++ storage_service/userapp/models.py
@@ -4,7 +4,18 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
-    phone = PhoneNumberField('Номер телефона', region='RU')
-    address = models.CharField('Адрес доставки', max_length=100)
-    avatar = models.FileField('Фото', null=True, blank=True)
+    phone = PhoneNumberField(
+        'Номер телефона',
+        region='RU'
+    )
+    address = models.CharField(
+        'Адрес доставки',
+        max_length=100
+    )
+    avatar = models.FileField(
+        'Фото',
+        upload_to='Media',
+        null=True, 
+        blank=True
+    )
 
